40a75c57384ee2cc5a803cffbb828d2230806255
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-22 10:17:58 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
-def reverse_each_word(word)
-  word.each do |word|
+def reverse_each_word(string)
+  string.split(",")
+    string.each do |word|
     puts word.reverse
+    return string.join(" ")
   end
 end
