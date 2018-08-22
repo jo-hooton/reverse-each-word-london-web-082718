cfe50483e1a618640f59cdd016a2daefe889413c
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-22 10:24:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,5 @@
 string = "Hello there, and how are you?"
 
 def reverse_each_word(string)
-  string.split(",")
-    string.collect do |word|
-    puts word.reverse
-    return string.join(" ")
-  end
+  string.split.collect { |word| word.reverse }.join(" ")
 end
